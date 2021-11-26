432875ddf0467610377a6c1973bb5cf11a1950bd
test-dan-run <daniel.leongsj@gmail.com>
2021-11-26 15:04:18 +0800

    update compose_transformation and add sample checkers

--- augdino/__init__.py
+++ augdino/__init__.py
@@ -7,17 +7,18 @@ from .wave.polarity_inversion import PolarityInversion
 from .wave.reverse_overlay import ReverseOverlay
 from .wave.shift import Shift
 from .wave.reverb import Reverb
+from .wave.pitch_shift import PitchShift
 
 from .spectrogram.freq_mask import CustomFrequencyMasking as FreqMask
 from .spectrogram.time_mask import CustomTimeMasking as TimeMask
 from .spectrogram.time_stretch import CustomTimeStretch as TimeStretch
 
+from .common.common import CustomMelSpectrogram
 from .utils.compose import Compose
 
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 
-AUGMENTATIONS = {
-    # wave
+WAVE_AUGMENTATIONS = {
     'background_noise': AddBackgroundNoise,
     'clip_distortion': ClipDistortion,
     'gain': Gain,
@@ -27,18 +28,34 @@ AUGMENTATIONS = {
     'reverse_overlay': ReverseOverlay,
     'shift': Shift,
     'reverb': Reverb,
-    # spectrogram
+    'pitch_shift': PitchShift
+}
+
+SPEC_AUGMENTATIONS = {
     'freq_mask': FreqMask,
     'time_mask': TimeMask,
     'time_stretch': TimeStretch,
 }
 
-def compose_augmentation(augment_list: List[str], augment_configs: Dict[str, Any]):
+def compose_transformations(
+    wave_augment_cfg: List[Dict[str, Any]],
+    mel_transform_cfg: Optional[Dict[str, Any]] = None,
+    spec_augment_cfg: Optional[List[Dict[str, Any]]] = None,
+    ) -> Compose:
     
     compose_list = []
 
-    for aug_name in augment_list:
-        transformation = AUGMENTATIONS[aug_name](**augment_configs[aug_name])
+    for aug_name in wave_augment_cfg.keys():
+        transformation = WAVE_AUGMENTATIONS[aug_name](**wave_augment_cfg[aug_name])
         compose_list.append(transformation)
+
+    if mel_transform_cfg is not None:
+        compose_list.append(CustomMelSpectrogram(**mel_transform_cfg))
+
+    if spec_augment_cfg is not None:
+        assert mel_transform_cfg is not None, 'Spectrogram augmentation(s) is being added when no melspectrogram configs are in place.'
+        for aug_name in spec_augment_cfg.keys():
+            transformation = SPEC_AUGMENTATIONS[aug_name](**spec_augment_cfg[aug_name])
+            compose_list.append(transformation)
 
     return Compose(compose_list)
